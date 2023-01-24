c0222d3fb8284502ac10181a46aec341498b877c
jbrazjr.es <jbrazjr.es@gmail.com>
2023-01-24 17:01:35 -0300

    Meu portfolio

--- src/index.js
+++ src/index.js
@@ -2,10 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ChakraProvider, extendTheme  } from '@chakra-ui/react'
+
+const theme = extendTheme({
+  styles: {
+    global: () => ({
+      body: {
+        bg: '#222831',
+      }
+    })
+  },
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
